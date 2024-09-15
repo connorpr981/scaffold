5af4c93959ea5b53a6464caa41faf0cc0a143db3
connorpr981 <connorpaulryan@gmail.com>
2024-09-14 23:37:30 -0400

    removed comments in strings

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -8,7 +8,7 @@ export default function SignIn() {
     <div className="flex items-center justify-center min-h-screen bg-background">
       <div className="p-6 max-w-sm w-full bg-card shadow-md rounded-md">
         <h1 className="text-2xl font-semibold text-center text-foreground mt-8 mb-6">
-          Sign in to Scaffold // Updated project name
+          Sign in to Scaffold
         </h1>
         <div className="mt-8">
           <Button
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Header from '../components/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Scaffold', // Updated project name
+  title: 'Scaffold',
   description: 'A template app with NextAuth and Vercel Postgres',
 }
 
