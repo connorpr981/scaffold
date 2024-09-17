62062a58ff62fa58c96d6d42b6d4181db45511d1
connorpr981 <connorpaulryan@gmail.com>
2024-09-17 00:17:19 -0400

    fix/finished the quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         </CardHeader>
         <CardContent className="text-center">
           <p className="mb-6 text-muted-foreground">
-            Imitation is the sincerest form of flattery.
+          Imitation is the sincerest form of flattery that mediocrity can pay to greatness.
           </p>
           {status === "authenticated" ? (
             <Button asChild>
